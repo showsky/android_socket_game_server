c3c150c8f263c5845d6d0d7ccd4680005fa5ad3f
tedshd <tedshd@gmail.com>
2013-10-30 16:38:25 +0800

    [Dev|yahoo_hack] Add music.

--- assets/javascripts/layout.js
+++ assets/javascripts/layout.js
@@ -6,7 +6,7 @@
  * @version $Id$
  */
 
-// $(function () {
+$(function () {
     // static data
     var profile = {
             'facebook_id': '123456',
@@ -50,7 +50,7 @@
                 $('.reciprocal').html(count);
                 if (count === 0) {
                     // $('.reciprocal').html('');
-                    window.game.start();
+                    // window.game.start();
                     clearInterval(rec);
                 }
                 count--;
@@ -62,23 +62,24 @@
     // addPlayer
     function addPlayer(data) {
         console.log(data);
-        var user = JSON.parse(data),
-            avatar,
+        var avatar,
             name,
             win,
             lose,
             f_id;
 
-            f_id = user.facebook_id;
-            avatar = user.facebook_avatar;
-            name = user.facebook_name;
-            win = user.win;
-            lose = user.lose;
+            f_id = data.facebook_id;
+            avatar = data.facebook_avatar;
+            name = data.facebook_name;
+            win = data.win;
+            lose = data.lose;
             $('.avatar:eq(' + n + ') img').attr('src', avatar).parent('.avatar').attr('id', f_id);
             $('.name:eq(' + n + ')').html(name);
             $('.rank:eq(' + n + ')').html(win + ' Win | ' + lose + ' Lose');
             if (n === 3) {
-                finishAdd();
+                // setTimeout(function () {
+                    finishAdd();
+                // }, 10000);
             }
             n++;
     }
@@ -90,6 +91,7 @@
         $('#player').removeClass('hide');
         $('.ans_bg').removeClass('hide');
         $('.loading').addClass('hide');
+        $('.attachment').remove();
     }
 
     // showQuestion and begin 3210 call start
@@ -98,16 +100,14 @@
         $('.hexagonal_ans').removeClass('dark');
         $('.ans').removeClass('correct');
         $('.avatar').removeClass('dark');
+        console.log(data);
 
         var n = 0,
-            question,
             x,
             q,
             a_array;
-        question = JSON.parse(data);
-        console.log("QUESTION:\n"+data);
-        q = question.question;
-        a_array = question.options;
+        q = data.question_lib.question;
+        a_array = data.question_lib.options;
         $('.question_area').html(q);
         for (x in a_array) {
             $('.ans:eq(' + n + ')').html(a_array[x]);
@@ -133,13 +133,21 @@
             // win UI
             $('.ans:eq(' + data.answer + ')').addClass('correct');
             // play music
+            $('body').append(
+                '<audio class="win" autoplay>' +
+                    '<source src="ff_win.mp3" type="audio/mpeg">' +
+                '</audio>'
+            );
+            setTimeout(function () {
+                $('.win').remove();
+            }, 5000);
         } else {
             // fail
             // update UI(fail user dark)
             $('.hexagonal_ans:eq(' + data.answer + ')').addClass('dark');
             $('.avatar').removeClass('dark');
             $('#' + fail_id + '').addClass('dark');
-            // reciprocal();
+            reciprocal();
         }
     }
 
@@ -159,106 +167,106 @@
     //     result(ok);
     // }, 10000);
 
-// });
+});
 
 
 
-// if (document.getElementById('player2')) {
-//     // get youtube api
-//     var tag = document.createElement('script'),
-//         firstScriptTag = document.getElementsByTagName('script')[0];
-//     tag.src = "https://www.youtube.com/iframe_api?client=181113669792.apps.googleusercontent.com&key=AIzaSyAqP-AGifepfLUe_XqZDbC75B3e4xYmVlk";
-//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+if (document.getElementById('player2')) {
+    // get youtube api
+    var tag = document.createElement('script'),
+        firstScriptTag = document.getElementsByTagName('script')[0];
+    tag.src = "https://www.youtube.com/iframe_api?client=181113669792.apps.googleusercontent.com&key=AIzaSyAqP-AGifepfLUe_XqZDbC75B3e4xYmVlk";
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-//     var playList = [],
-//         playCount = 0,
-//         player,
-//         onPlayerStateChange,
-//         value,
-//         key;
+    var playList = [],
+        playCount = 0,
+        player,
+        onPlayerStateChange,
+        value,
+        key;
 
-//     var video_ID = {
-//         'a_0': 'f-FOf8RRq-U',
-//         'a_1': '7wvNwOPprBE',
-//         'a_2': 'LyMjaZE0GE8',
-//         'a_3': 'xWTiOqJqkk0',
-//         'a_4': 'F2uX6ByoW7A',
-//         'a_5': 'LWmVK8K2QhM',
-//         'a_6': 'mTSuiGubCHE'
-//     };
+    var video_ID = {
+        'a_0': 'f-FOf8RRq-U',
+        'a_1': '7wvNwOPprBE',
+        'a_2': 'LyMjaZE0GE8',
+        'a_3': 'xWTiOqJqkk0',
+        'a_4': 'F2uX6ByoW7A',
+        'a_5': 'LWmVK8K2QhM',
+        'a_6': 'mTSuiGubCHE'
+    };
 
-//     // playList array
-//     for (key in video_ID) {
-//         value = video_ID[key];
-//         console.log(value);
-//         playList.push(value);
-//     }
+    // playList array
+    for (key in video_ID) {
+        value = video_ID[key];
+        console.log(value);
+        playList.push(value);
+    }
 
-//     function playChannel() {
-//         // init player
-//         player = new YT.Player('player', {
-//             height: '350',
-//             width: '480',
-//             videoId: playList[0],
-//             playerVars: {
-//                 rel: 1,
-//                 autoplay: 0,
-//                 disablekb: 0,
-//                 showsearch: 0,
-//                 showinfo: 0,
-//                 controls: 0,
-//                 wmode: 'opaque',
-//                 hd: 1,
-//                 iv_load_policy: 3,
-//                 start: 60
-//             },
-//             events: {
-//                 'onReady': onPlayerReady,
-//                 'onStateChange': onPlayerStateChange
-//             }
-//         });
+    function playChannel() {
+        // init player
+        player = new YT.Player('player', {
+            height: '350',
+            width: '480',
+            videoId: playList[0],
+            playerVars: {
+                rel: 1,
+                autoplay: 0,
+                disablekb: 0,
+                showsearch: 0,
+                showinfo: 0,
+                controls: 0,
+                wmode: 'opaque',
+                hd: 1,
+                iv_load_policy: 3,
+                start: 60
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
 
-//         // play video
-//         function onPlayerReady(event) {
-//             event.target.playVideo();
-//         }
-//     }
+        // play video
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+    }
 
-//     // play list loop
-//     function onPlayerStateChange(event) {
-//         console.log(event);
-//         if (event.data === YT.PlayerState.PLAYING) {
-//             setTimeout(
-//                 function () {
-//                     player.pauseVideo();
-//                 },
-//                 10000
-//             );
-//         }
-//         if (event.data === 0) {
-//             console.log('Next');
-//             playCount++;
-//             if (playCount > (playList.length - 1)) {
-//                 playCount = 0;
-//             }
-//             player.loadVideoById(playList[playCount]);
-//             player.playVideo();
-//         }
-//     }
+    // play list loop
+    function onPlayerStateChange(event) {
+        console.log(event);
+        if (event.data === YT.PlayerState.PLAYING) {
+            setTimeout(
+                function () {
+                    player.pauseVideo();
+                },
+                10000
+            );
+        }
+        if (event.data === 0) {
+            console.log('Next');
+            playCount++;
+            if (playCount > (playList.length - 1)) {
+                playCount = 0;
+            }
+            player.loadVideoById(playList[playCount]);
+            player.playVideo();
+        }
+    }
 
-//     function onYouTubeIframeAPIReady() {
-//         $(function () {
-//             console.log('jQuery');
-//             playChannel();
-//             $('button').on('click', function () {
-//                 playList = ['DLFPqJedFuw', 'MmHkPXG4Td4'];
-//                 // update play list
-//                 // playList =
-//                 player.loadVideoById(playList[0]);
-//                 onPlayerStateChange();
+    function onYouTubeIframeAPIReady() {
+        $(function () {
+            console.log('jQuery');
+            playChannel();
+            $('button').on('click', function () {
+                playList = ['DLFPqJedFuw', 'MmHkPXG4Td4'];
+                // update play list
+                // playList =
+                player.loadVideoById(playList[0]);
+                onPlayerStateChange();
 
-//             });
+            });
 
-//         });
-//     }
-// }
+        });
+    }
+}
